79d99d927825793cf3fabdc687587a26e549b868
Dan Stoppelman <dstop75@gmail.com>
2015-01-21 23:56:24 -0500

    1st commit

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+1.upto(5) do |num|   Car.create(make: "make_#{num}", model: "model_#{num}", year: 2009 + num) end
+
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
